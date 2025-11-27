7813d81c08a88e3a0f7c9064c7e23a9e3f06e523
hashimow <hashimow01@gmail.com>
2025-11-27 12:35:42 +0300

    lab nested functions

--- index.js
+++ index.js
@@ -1,3 +1,76 @@
+function createLoginTracker(userInfo) {
+
+    // start the login attempts at 0
+    let attemptCount = 0;
+
+    // this will check the password each time
+    const tryLogin = (passwordAttempt) => {
+
+        // increase attempts
+        attemptCount = attemptCount + 1;
+
+        // if attempts are more than 3, lock account
+        if (attemptCount > 3) {
+            return "Account locked due to too many failed login attempts";
+        }
+
+        // check if the password is correct
+        if (passwordAttempt === userInfo.password) {
+            return "Login successful";
+        } else {
+            return "Attempt " + attemptCount + ": Login failed";
+        }
+    };
+
+    // return the inner function
+    return tryLogin;
+}
+const login = createLoginTracker({
+    username: "user1",
+    password: "password123"
+});
+
+console.log(login("abc"));        // Attempt 1: Login failed
+console.log(login("hello"));      // Attempt 2: Login failed
+console.log(login("1234"));       // Attempt 3: Login failed
+console.log(login("password123")); // Account locked due to too many failed login attempts
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
